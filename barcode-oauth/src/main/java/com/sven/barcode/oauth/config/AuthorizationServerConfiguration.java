ff074cca5fe8684ba7a22a856a7509a419fd62ea
Kwan <kwan@bmj.com>
2017-08-08 10:08:38 +0100

    move static clients to yml file

--- barcode-oauth/src/main/java/com/sven/barcode/oauth/config/AuthorizationServerConfiguration.java
+++ barcode-oauth/src/main/java/com/sven/barcode/oauth/config/AuthorizationServerConfiguration.java
@@ -1,6 +1,7 @@
 package com.sven.barcode.oauth.config;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -11,6 +12,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.ClientRegistrationException;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 
@@ -19,22 +24,44 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter
 {
 
-    @Autowired
-    private TokenStore tokenStore;
+    @Autowired @Qualifier("oauth-static-clients") private List<BaseClientDetails> staticClients;
 
-    @Autowired
-    @Qualifier("authenticationManagerBean")
-    private AuthenticationManager authenticationManager;
+    @Autowired private TokenStore tokenStore;
+
+    @Autowired @Qualifier("authenticationManagerBean") private AuthenticationManager authenticationManager;
 
     @Override
     public void configure(final ClientDetailsServiceConfigurer clients) throws Exception
     {
-        clients.inMemory().withClient("implicitClientId").authorizedGrantTypes(
-                "implicit").scopes("read").autoApprove(true).and().withClient(
-                        "passwordClientId").secret("secret").authorizedGrantTypes(
-                                "password", "authorization_code", "refresh_token").scopes(
-                                        "read").accessTokenValiditySeconds(
-                                                120).refreshTokenValiditySeconds(120);
+        clients.withClientDetails(new ClientDetailsService()
+        {
+
+            @Override
+            public ClientDetails loadClientByClientId(final String clientId)
+                    throws ClientRegistrationException
+            {
+
+                return staticClients.stream()
+                                    .filter(s -> s.getClientId().equalsIgnoreCase(clientId))
+                                    .findAny()
+                                    .orElseThrow(
+                                            () -> new ClientRegistrationException(clientId));
+            }
+
+        });
+
+        // clients.inMemory()
+        // .withClient("implicitClientId")
+        // .authorizedGrantTypes("implicit")
+        // .scopes("read")
+        // .autoApprove(true)
+        // .and()
+        // .withClient("passwordClientId")
+        // .secret("secret")
+        // .authorizedGrantTypes("password", "authorization_code", "refresh_token")
+        // .scopes("read")
+        // .accessTokenValiditySeconds(120)
+        // .refreshTokenValiditySeconds(120);
     }
 
     @Override
@@ -43,9 +70,10 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     {
         final TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
         tokenEnhancerChain.setTokenEnhancers(Arrays.asList(new SimpleTokenEnhancer()));
-        endpoints.tokenStore(tokenStore).tokenEnhancer(tokenEnhancerChain)
-                // .userApprovalHandler(userApprovalHandler)
-                .authenticationManager(authenticationManager);
+        endpoints.tokenStore(tokenStore)
+                 .tokenEnhancer(tokenEnhancerChain)
+                 // .userApprovalHandler(userApprovalHandler)
+                 .authenticationManager(authenticationManager);
     }
 
     @Override
---
+++ barcode-oauth/src/main/java/com/sven/barcode/oauth/config/OauthStaticClientConfig.java
@@ -0,0 +1,33 @@
+package com.sven.barcode.oauth.config;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
+
+@Configuration
+@ConfigurationProperties(prefix = "oauth.static")
+public class OauthStaticClientConfig
+{
+
+    private List<BaseClientDetails> clients;
+
+
+    public List<BaseClientDetails> getClients()
+    {
+        return clients;
+    }
+
+    public void setClients(final List<BaseClientDetails> clients)
+    {
+        this.clients = clients;
+    }
+    
+    @Bean("oauth-static-clients")
+    public List<BaseClientDetails> clients()
+    {
+        return clients;
+    }
+}
